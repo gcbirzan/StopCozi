b7ac9b9f8a81f17e31b852a9f292a22d0e1ea827
Andrei Balan <andrei@Andreis-MacBook-Pro.local>
2016-10-08 19:46:01 +0300

    MVP html base

--- frontend/js/application/controllers/common/ReservationController.js
+++ frontend/js/application/controllers/common/ReservationController.js
@@ -2,6 +2,33 @@ var CommonReservationController = ['$controller', '$scope', '$rootScope', '$stat
     function ($controller, $scope, $rootScope, $state, CONFIG, $http, apiUrlFactory, aclFactory, translationFactory) {
         $controller('ParentController', {$scope: $scope});
 
+        $scope.countyList = [{
+            id: 'BH',
+            name: 'Bihor'
+        }, {
+            id: 'GL',
+            name: 'Galati'
+        }];
+
+        $scope.agencies = [      {
+        		parent: 'BH',
+            id: 1,
+            name: "SPCLEP",
+            description: "Evidenta Populatiei",
+            location: "BH",
+            geo: "2355,46767",
+            address: "Cicoarei 17",
+            contact:"tel: 07346"
+          },{
+          	parent: 'GL',
+            id: 2,
+            name: "Pasapoarte",
+            description: "Biroul Pasapoarte",
+            location: "GL",
+            geo: "6757,63467",
+            address: "Prezan 12",
+            contact:"tel: 07346"
+          }];
         $rootScope.pageTitle = translationFactory.translate('common.reservation.title|Bine ai venit');
 
 
