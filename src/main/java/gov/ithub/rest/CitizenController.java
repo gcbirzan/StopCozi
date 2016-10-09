a7e2938fcf817ebe362d3553e2ccd41f6e5a8087
Nicolae Botez <nicolaebotez@yahoo.com>
2016-10-09 10:10:20 +0300

    Merge branch 'master' into likeName
    Conflicts:
    	src/main/java/gov/ithub/dao/ServiceDao.java

--- src/main/java/gov/ithub/rest/CitizenController.java
+++ src/main/java/gov/ithub/rest/CitizenController.java
@@ -11,10 +11,7 @@ import gov.ithub.service.FreeSlotService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Date;
