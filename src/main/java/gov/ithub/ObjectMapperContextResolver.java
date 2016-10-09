933bb0f4be5bb95acdf260ecc51f6ed71cd40454
Claudiu Barbieru <claudiu.barbieru@basware.com>
2016-10-09 09:54:08 +0300

    added the API for creating appointments

--- src/main/java/gov/ithub/ObjectMapperContextResolver.java
+++ src/main/java/gov/ithub/ObjectMapperContextResolver.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 
 /**
  * Created by claudiubar on 10/8/2016.
@@ -16,6 +19,9 @@ public class ObjectMapperContextResolver implements ContextResolver<ObjectMapper
 
     public ObjectMapperContextResolver() {
         mapper = new ObjectMapper();
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        df.setTimeZone(TimeZone.getTimeZone("UTC"));
+        mapper.setDateFormat(df);
         mapper.setPropertyNamingStrategy(
                 PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES
         );
