faf29d00ed5719e741ac0c8ccf2f8ffb829e4fd7
Bogdan Vlad <vladbgd@gmail.com>
2016-10-09 13:59:17 +0300

    Send SMS when an appointment is created.

--- src/main/java/gov/ithub/service/AppointmentService.java
+++ src/main/java/gov/ithub/service/AppointmentService.java
@@ -9,6 +9,13 @@ import gov.ithub.model.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -20,7 +27,9 @@ import java.util.stream.Collectors;
 @Component
 public class AppointmentService {
 
-    static final long ONE_MINUTE_IN_MILLIS=60000;//millisecs
+    static final long ONE_MINUTE_IN_MILLIS = 60000;
+    static final String SMS_SERVICE_API_SECRET_ENV = "STOP_COZI_API_SECRET";
+    static final String SMS_SERVICE_API_KEY_ENV = "STOP_COZI_API_KEY";
 
     @Autowired
     private AppointmentDao appointmentDao;
@@ -30,8 +39,35 @@ public class AppointmentService {
 
     @Autowired
     private OfficeDao officeDao;
+    
+    private String formatDate(Date date) {
+    	return new SimpleDateFormat("dd/MM/yyyy HH:mm").format(date);
+    }
+    
+    private String getSMSContent(String name, String service, String data, long code) {
+    	return "Vesti bune" + name + ", programarea Dvoastra la " + service + " pe data de " + data + " este confirmata. Codul programarii este " + code + ".";
+    }
+    
+    private void sendSMS(String message, String to, String from) throws Exception {
+        StringBuilder result = new StringBuilder();
+        String apiSecret = System.getenv(SMS_SERVICE_API_SECRET_ENV);
+        String apiKey = System.getenv(SMS_SERVICE_API_KEY_ENV);
+        String urlString = "https://rest.nexmo.com/sms/json?api_key=" + apiKey + "&api_secret=" + apiSecret + "&from=" + from + "&to=" + to + "&text=" + URLEncoder.encode(message, "UTF-8");
+        
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        
+        while ((line = rd.readLine()) != null) {
+           result.append(line);
+        }
+        
+        rd.close();
+    }
 
-    public Optional<Appointment> createAppointment(Long serviceId, Appointment appointment){
+	public Optional<Appointment> createAppointment(Long serviceId, Appointment appointment){
         Service service = serviceDao.findById(serviceId);
         Date start = appointment.getStart();
         Date end = new Date(start.getTime() + (service.getDuration() * ONE_MINUTE_IN_MILLIS));
@@ -45,8 +81,21 @@ public class AppointmentService {
         Optional<Office> firstFreeOffice = offices.stream().filter(o -> !busyOffices.contains(o.getId())).findFirst();
         if(firstFreeOffice.isPresent()) {
             appointment.setOffice(firstFreeOffice.get());
-            return Optional.of(appointmentDao.save(appointment));
-        } else
-            return Optional.empty();
+            Optional<Appointment> createdAppointment = Optional.of(appointmentDao.save(appointment));
+            
+            if (createdAppointment.isPresent()) {
+            	try {
+            		Appointment app = createdAppointment.get();
+            		
+            		sendSMS(getSMSContent(app.getName(), service.getName(), formatDate(app.getStart()), app.getId()), app.getPhone(), "StopCozi");
+            	} catch (Exception e) {
+            		
+            	}
+            	
+            	return createdAppointment;
+            }
+        }
+           
+        return Optional.empty();
     }
 }
